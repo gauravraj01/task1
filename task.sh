b3945326aa09ca9064db20183927d677bc90d500
gauravraj01 <55096722+gauravraj01@users.noreply.github.com>
2021-12-29 22:59:53 +0530

    Add files via upload

---
+++ task.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+echo "Enter text"
+read text
+
+# count number of characters
+TotalCharacters=$(echo -n "$text" |grep -o '[a-z]' | wc -l)
+
+
+# count number of words
+word=$(echo -n "$text" | wc -w)
+
+
+# Counting Number of total space
+space=$(echo -n "$text" |grep -o ' ' | wc -l)
+
+
+
+# Counting total digits
+digits=$(echo -n "$text" | awk -F '[0-9]' '{print NF-1}')
+
+echo "TotalSpace-$space;TotalCharacters-$TotalCharacters;TotalWords-$word;TotalDigits-$digits"
